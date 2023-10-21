fd6bf3dd7197fcb3c22d255766fa2c5e081cdf1b
nadisan <nadiawellela@gmail.com>
2023-10-21 04:50:16 -0700

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,11 @@ def hello():
     """display Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """display HBNB"""
-    return 'HBNB!'
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
