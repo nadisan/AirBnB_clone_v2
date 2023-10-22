2ac3916dc2d7eef27adab098048cd135fc62987f
nadisan <nadiawellela@gmail.com>
2023-10-21 22:26:05 -0700

    task5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,7 @@ def imanumber(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def displayhtml(n):
+def numbersandtemplates(n):
     """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
