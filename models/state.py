56041823745389f133ed54ebee4aadc1549b7271
nadisan <nadiawellela@gmail.com>
2023-11-05 05:23:15 -0800

    task

---
+++ models/state.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer
+from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+import os
+import models
+from models.city import City  # Import the City class
+import shlex
+
+
+from models.base_model import Base
+
+
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = "states"
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade='all, delete, delete-orphan',
+                              back_populates="state")
+
+    else:
+        @property
+        def cities(self):
+            var = models.storage.all()
+            lista = []
+            result = []
+            for key in var:
+                city = key.replace('.', ' ')
+                city = shlex.split(city)
+                if (city[0] == 'City'):
+                    lista.append(var[key])
+            for elem in lista:
+                if (elem.state_id == self.id):
+                    result.append(elem)
+            return result
