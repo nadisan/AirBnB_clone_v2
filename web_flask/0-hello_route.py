b85aaf52076b310e9d381f521e6c04a3f7d73702
nadia <nadiawellela@gmail.com>
2023-11-13 09:35:22 +0000

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,5 +11,10 @@ def hello():
     """display Hello HBNB!"""
     return 'Hello HBNB!'
 
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hellonew():
+    """display Hello HBNB!"""
+    return 'Hello HBNB!'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
